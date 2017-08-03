40f13d28e087d37dae599850fb6ebce84ae03eda
Mark Huot <mark@markhuot.com>
2017-08-03 14:27:34 -0400

    checking bash envs

--- bin/setup/travis.php
+++ bin/setup/travis.php
@@ -1,7 +1,7 @@
 <?php
 
 // add craftql to the deps as a symbolic link
-$package = file_get_contents('/tmp/craft3/composer.json');
+$package = file_get_contents('/home/travis/build/craftcms/craft/composer.json');
 $package = json_decode($package, true);
 $package['repositories'] = @$package['repositories'] ?: [];
 $package['repositories'][] = [
@@ -9,4 +9,4 @@ $package['repositories'][] = [
     "url" => realpath(dirname(__FILE__)."/../../"),
 ];
 $package['minimum-stability'] = 'dev';
-file_put_contents('/tmp/craft3/composer.json', json_encode($package));
+file_put_contents('/home/travis/build/craftcms/craft/composer.json', json_encode($package));
