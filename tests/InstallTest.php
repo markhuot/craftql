d06f25bbbbcc5189a72a5f597c711d79f33e210e
Mark Huot <mark@markhuot.com>
2017-08-03 14:12:41 -0400

    fixing travis

---
+++ tests/InstallTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers Email
+ */
+final class InstallTest extends TestCase
+{
+    public function testPhpUnitIsWorking(): void
+    {
+        $this->assertEquals(1, 1);
+    }
+}
