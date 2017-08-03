581d7235bfb3c87b5b13eb7938f6b08f91a0b0f5
Mark Huot <mark@markhuot.com>
2017-08-03 13:57:01 -0400

    tweaking travis

--- bin/setup/travis.php
+++ bin/setup/travis.php
@@ -1,9 +1,5 @@
 <?php
 
-// clone the craft repo
-`rm -rf /tmp/craft3`;
-`composer create-project craftcms/craft /tmp/craft3`;
-
 // add craftql to the deps as a symbolic link
 $package = file_get_contents('/tmp/craft3/composer.json');
 $package = json_decode($package, true);
@@ -14,6 +10,3 @@ $package['repositories'][] = [
 ];
 $package['minimum-stability'] = 'dev';
 file_put_contents('/tmp/craft3/composer.json', json_encode($package));
-
-// require craftql, through a symbolic link
-`pushd /tmp/craft3 && composer require markhuot/craftql dev-master && composer require phpunit`;
