714b3441d856e63da805ddec712dbaaead3ef717
Mark Huot <mark@markhuot.com>
2017-12-01 08:06:00 -0500

    ability to create new tags on insert

--- src/Fields/TagsBehavior.php
+++ src/Fields/TagsBehavior.php
@@ -3,17 +3,36 @@
 namespace markhuot\CraftQL\Fields;
 
 use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\InputObjectType;
 use GraphQL\Type\Definition\Type;
 use yii\base\Behavior;
+use craft\elements\Tag;
+use Craft;
 
 class TagsBehavior extends Behavior
 {
+
+    static $inputs = [];
+
+    function getGraphQLInput($field) {
+        if (isset(static::$inputs[$field->handle])) {
+            return static::$inputs[$field->handle];
+        }
+
+        return static::$inputs[$field->handle] = new InputObjectType([
+            'name' => ucfirst($field->handle).'TagInput',
+            'fields' => [
+                'id' => Type::int(),
+                'title' => Type::string(),
+            ],
+        ]);
+    }
 
     public function getGraphQLMutationArgs() {
         $field = $this->owner;
 
         return [
-            $field->handle => ['type' => Type::listOf(Type::int())]
+            $field->handle => ['type' => Type::listOf($this->getGraphQLInput($field))]
         ];
     }
 
@@ -43,7 +62,28 @@ class TagsBehavior extends Behavior
     }
 
     public function upsert($values) {
-        return $values;
+        return array_map(function ($value) {
+            if ($id = @$value['id']) {
+                return $id;
+            }
+
+            $field = $this->owner;
+            preg_match('/taggroup:(\d+)/', $field->source, $matches);
+            if (!$matches) {
+                return;
+            }
+
+            $groupId = $matches[1];
+            $group = Craft::$app->tags->getTagGroupById($groupId);
+
+            $tag = new Tag();
+            $tag->groupId = $groupId;
+            $tag->fieldLayoutId = $group->fieldLayoutId;
+            $tag->title = trim(@$value['title']);
+            Craft::$app->getElements()->saveElement($tag);
+
+            return $tag->id;
+        }, $values);
     }
 
 }
