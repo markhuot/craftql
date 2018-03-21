cf4c55679d06cab011a4129e7201ff917498fa19
Mark Huot <mark@markhuot.com>
2018-03-21 16:03:57 -0400

    fixes named image transforms

--- src/FieldBehaviors/AssetTransformArguments.php
+++ src/FieldBehaviors/AssetTransformArguments.php
@@ -22,7 +22,10 @@ class AssetTransformArguments extends FieldBehavior {
         $values = [];
 
         foreach (\Craft::$app->getAssetTransforms()->getAllTransforms() as $transform) {
-            $values[$transform->handle] = $transform->name;
+            $values[$transform->handle] = [
+                'value' => $transform->handle,
+                'description' => $transform->name
+            ];
         }
 
         if (empty($values)) {
