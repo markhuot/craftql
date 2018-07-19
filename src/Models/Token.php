cc61f852ad95c1a473ebec759cfc99259460ee8a
Jan Thoma <jan.thoma@t-k-f.ch>
2018-07-19 12:43:12 +0200

    unnessecary try catch block

--- src/Models/Token.php
+++ src/Models/Token.php
@@ -4,7 +4,6 @@ namespace markhuot\CraftQL\Models;
 
 use Craft;
 use craft\db\ActiveRecord;
-use Firebase\JWT\ExpiredException;
 use GraphQL\Error\UserError;
 use markhuot\CraftQL\CraftQL;
 
@@ -59,12 +58,7 @@ class Token extends ActiveRecord
 
         // If the token matches a JWT format
         if (preg_match('/[^.]+\.[^.]+\.[^.]+/', $token)) {
-            try {
-                $tokenData = CraftQL::getInstance()->jwt->decode($token);
-            }
-            catch (ExpiredException $e) {
-                throw new UserError('The token has expired');
-            }
+            $tokenData = CraftQL::getInstance()->jwt->decode($token);
             $user = \craft\elements\User::find()->id($tokenData->id)->one();
             $token = Token::forUser($user);
             return $token;
