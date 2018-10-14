6409afcd1db620ada445ce8b6a89740f8d669e01
Narration SD <narrationsd@gmail.com>
2018-10-13 17:04:06 -0700

    Completed @idNot directive ability

--- src/Builders/Field.php
+++ src/Builders/Field.php
@@ -7,6 +7,7 @@ use GraphQL\Executor\Executor;
 use GraphQL\Utils\Utils;
 use markhuot\CraftQL\Behaviors\FieldBehavior;
 use markhuot\CraftQL\Request;
+use craft\helpers\Db;
 
 class Field extends BaseBuilder
 {
@@ -18,8 +19,6 @@ class Field extends BaseBuilder
     use HasResolveAttribute;
     use HasArgumentsAttribute;
     use HasDeprecationReasonAttribute;
-
-//    protected $arguments = [];
 
     function __construct(Request $request, string $name)
     {
@@ -95,76 +94,172 @@ class Field extends BaseBuilder
         return $this->onSave;
     }
 
-    function getResolve() {
-
-        // a critical alternative here, to stay out of the schema build
-        // at present, Field is the place we want to hit for list directive
+    /**
+     * @return \Closure|mixed
+     */
+    function getResolve()
+    {
+        // A critical alternative here, to stay out of the schema build.
+        // The anonymous function is needed because PHP, but cleaner to see
         return $this->resolve
             ? $this->resolve
-            : function ($root, $args, $context, $info) {
+            : function($root, $args, $context, $info) {
+                return $this->listResolve($root, $args, $context, $info);
+            };
+    }
 
-            $inVals = '0';
-            $listField = 'children';
+    public function listResolve($root, $args, $context, $info)
+    {
 
-            if (isset($info->fieldNodes[0]->directives[0])) {
+        // At present, Field is the Builder we want to hit, for
+        // directives which appear on fields which are actually lists,
+        // which at present include entries, and several more.
 
-                $directive = $info->fieldNodes[0]->directives[0];
+        // first, safeties, for what shoudldn't occur
+        if (is_null($root)) {
+            return Utils::undefined();
+        }
+
+        $field = $root->{$info->fieldName};
+        if (!$field) {
+            return null;
+        }
+
+        // now, handle each possible directive, first assessing their arguments
+
+        if (isset($info->fieldNodes[0]->directives[0])) {
+            foreach ($info->fieldNodes[0]->directives as $directive) {
                 if ($directive->arguments) {
                     foreach ($directive->arguments as $arg) {
+                        $argValues = $this->valuesFromArguments($info, $arg, $directive);
 
-                        $argValues = null;
+                        switch ($directive->name->value) {
 
-                        if ($arg->value->kind === 'Argument') {
-                            // so far for array case, untied below
-                            $argValues = $arg->value->values;
-                        }
-                        else if ($arg->value->kind === 'Variable') {
-                            // unwind the variable
-                            $argValues = [];
-                            foreach($info->variableValues[$arg->value->name->value] as $value) {
-                                $argValues[] = (object)[ 'value' => $value ];
-                            }
-                        }
-                        else {
-                            throw new \Exception ('Unknown argument type for '
-                                . $arg->name->value . ' in '
-                                . $directive->name);
-                        }
-
-                        switch ($arg->name->value) {
-                            case 'in':
-                                $i = 0;
-                                // tricky here - don't point to internal of values
-                                foreach ($argValues as $value) {
-                                    $i++ === 0
-                                        ? $inVals = $value->value
-                                        : $inVals .= ', ' . $value->value;
-                                }
+                            case 'idNot':
+                                $field = $this->handleAtIdNot($field, $argValues, $directive, $arg);
                                 break;
 
-                            case 'field':
-                                $listField = $arg->value->value;
+                            // other field directives could be added here, with handler...
+
+                            default:
                                 break;
                         }
                     }
                 }
             }
+        }
 
-            if(is_null($root)) {
-                return Utils::undefined(); // safety; shouldn't occur
+        return $field;
+    }
+
+    /**
+     * @param $info
+     * @param $arg
+     * @param $directive
+     * @return array|null
+     * @throws \Exception
+     */
+    function valuesFromArguments($info, $arg, $directive)
+    {
+        $argValues = null;
+
+        if ($arg->kind === 'Argument') {
+            // so far for array case, untied below
+            switch ($arg->value->kind) {
+
+                case 'IntValue':
+                    // we want always to work with arrays
+                    $argValues = [$arg->value->value];
+                    break;
+
+                case 'StringValue':
+                    $argValues = [$arg->value->value];
+                    break;
+
+                case 'ListValue':
+                    $argValues = [];
+                    foreach ($arg->valus->nodes as $node) {
+                        $argValues[] = $node->value;
+                    }
+                    break;
+
+                case 'Variable':
+                    $argValues = [];
+                    foreach ($info->variableValues[$arg->value->name->value] as $value) {
+                        $argValues[] = $value;
+                    }
+                    break;
+
+                default:
+                    $msg = 'Unexpected argument value in directive: '
+                        . $directive->name;
+                    throw new \Exception($msg);
+                    break;
             }
+        } else {
+            throw new \Exception ('Unknown argument kind: '
+                . $arg->kind . ' for '
+                . $arg->name->value . ' in '
+                . $directive->name);
+        }
+        return $argValues;
+    }
 
-            $field = $root->{$info->fieldName};
-            if (!$field) {
-                return null; // safety; shouldn't occur
-            }
+    /**
+     *
+     * This is how a handler looks -- make new ones for new directives
+     *
+     * @param $field
+     * @param $argValues
+     * @param $directive
+     * @param $arg
+     * @return mixed
+     */
+    public function handleAtIdNot($field, $argValues, $directive, $arg)
+    {
+        $listField = ''; // temporary
+        $value = ''; // temporary
 
-            if ($info->fieldName === $listField) {
-                // *todo* hard line only from here? alternate databases...
-                $field->where = 'elements.id not in (' . $inVals . ')';
-            }
+        switch ($arg->name->value) {
 
-            return $field;
-        };
+            case 'in':
+                if ($inVals = $this->inValsFromValues($argValues)) {
+                    // only non-empty (@idNot in:) values arrays
+                    // and, this way matches formation of condition (not-@) idNot
+                    $helped = Db::parseParam('elements.id', $inVals, '!=');
+                    $field = $field->where
+                        ? $field->andWhere($helped)
+                        : $field->where($helped);
+                }
+                break;
+
+            // here other arguments for a directive would be handled similarly
+
+            default:
+                throw new \Exception('Unexpected argument: ' +
+                    $arg->name->value +
+                    ' in directive: ' + $directive->name);
+        }
+
+        return $field;
+    }
+
+    /**
+     * @param $argValues
+     * @param $inVals
+     * @return string
+     */
+    public function inValsFromValues($argValues): string
+    {
+        $i = 0;
+        $inVals = null;
+
+        foreach ($argValues as $value) {
+            $i++ === 0
+                ? $inVals = 'and, !=' . $value
+                : $inVals .= ', !=' . $value;
+        }
+
+        return $inVals;
     }
 }
