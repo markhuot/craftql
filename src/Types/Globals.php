8ed3a27292e77f0171f4a354150236c413ab4ff2
Mark Huot <mark@markhuot.com>
2019-02-23 19:28:16 -0500

    fixing global set empty fields

--- src/Types/Globals.php
+++ src/Types/Globals.php
@@ -10,13 +10,19 @@ class Globals extends Schema {
         return ucfirst($this->context->handle);
     }
 
-    protected function boot() {
+    function boot() {
         $this->addFieldsByLayoutId($this->context->fieldLayoutId);
+    }
+
+    function bootFieldLayouts(): Schema {
+        parent::bootFieldLayouts();
 
         if (empty($this->fields)) {
             $warning = 'The global set, `'.$this->getName().'`, has no fields. This would violate the GraphQL spec so we filled it in with this placeholder.';
             $this->addStringField('empty')->description($warning)->resolve($warning);
         }
+
+        return $this;
     }
 
     /**
